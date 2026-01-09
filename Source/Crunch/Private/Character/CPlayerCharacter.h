b26b15d4a6e7a6ff681e9198bbbf598c307edb48
rldiaz14 <rldiaz14.1@gmail.com>
2026-01-09 18:52:13 -0500

    Added Base Class
    Add Class

---
+++ Source/Crunch/Private/Character/CPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CCharacter.h"
+#include "CPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACPlayerCharacter : public ACCharacter
+{
+	GENERATED_BODY()
+	
+};
